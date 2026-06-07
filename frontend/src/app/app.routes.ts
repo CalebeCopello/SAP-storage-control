e98e9ca555215471b3ac71e8d87128996fe2bd4c
Calebe Copello <calebecopello@gmail.com>
2026-06-07 02:39:51 -0300

    chore: add route /user/register to UserRegister component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserRegister } from './features/auth/user-register/user-register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'user/register',
+        component: UserRegister
+    }
+];
