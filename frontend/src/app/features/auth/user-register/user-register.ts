d10aaf9b75af319af198d8987c78ffa37e8dfd67
Calebe Copello <calebecopello@gmail.com>
2026-06-07 02:39:16 -0300

    chore: create user-register component

---
+++ frontend/src/app/features/auth/user-register/user-register.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'sc-user-register',
+  imports: [
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './user-register.html',
+  styleUrl: './user-register.scss',
+})
+export class UserRegister {
+  registerForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+  ) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+  onSubmit(): void {
+    if (this.registerForm.invalid) return;
+
+    this.http.post(`${environment.apiUrl}/api/auth/register`, this.registerForm.value).subscribe({
+      next: (res) => console.log('Success: ', res),
+      error: (err) => console.error('Error: ', err),
+    });
+  }
+}
