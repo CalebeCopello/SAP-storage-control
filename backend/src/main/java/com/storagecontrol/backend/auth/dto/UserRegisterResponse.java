bbd0b02a4722a97c1620e0490081a2a3029f5231
Calebe Copello <calebecopello@gmail.com>
2026-06-12 00:09:24 -0300

    chore add static

--- backend/src/main/java/com/storagecontrol/backend/auth/dto/UserRegisterResponse.java
+++ backend/src/main/java/com/storagecontrol/backend/auth/dto/UserRegisterResponse.java
@@ -3,10 +3,22 @@ package com.storagecontrol.backend.auth.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.storagecontrol.backend.domain.User;
+
 public record UserRegisterResponse (
     UUID id,
     String name,
     String email,
     LocalDateTime createdAt,
     LocalDateTime updatedAt
-){}
+){
+    public static UserRegisterResponse from(User user) {
+        return new UserRegisterResponse(
+            user.getId(),
+            user.getName(),
+            user.getEmail(),
+            user.getCreatedAt(),
+            user.getUpdatedAt()
+        );
+    }
+}
